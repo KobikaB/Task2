0951dc9a8de4b86be59c92872ac7921cec2a97a3
kobika <bkobika06@gmail.com>
2025-04-22 00:33:52 +0530

    Updated App.jsx with new changes.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,98 +1,103 @@
 import React, { useState } from "react";
 
-const Form = ({Name}) => {
-  
- 
-  const [error, setError] = useState("");
-  
+
+const Form = ({ Name }) => {
   const [submitted, setSubmitted] = useState(false);
-  const [formData,setFormData] = useState({
-    name:"",
-    password:""
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
   });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    setError("");
-    setSubmitted(false);
+
    
-   try{
-    if(formData.name.trim() == "" || formData.password.length != 8) {
-      if(formData.name.trim() == ""){
-        setError("Name is require"); 
-      }else if(formData.password.length != 8){
-      setError("password must be exactly 8 charactors");
+    setNameError("");
+    setPasswordError("");
+    setSubmitted(false);
+
+    let isValid = true;
+
+    if (formData.name.trim() === "") {
+      setNameError("Name is required");
+      isValid = false;
     }
 
-   } else{
-    setSubmitted(true);
-   }
-   
-  }catch (err) {
-      setError("please try again");
-    } 
-    finally {
-      
-      
+    if (formData.password.length < 8) {
+      setPasswordError("Password must be minimum 8 characters");
+      isValid = false;
+    }
+
+    if (isValid) {
+      setSubmitted(true);
     }
   };
 
   return (
     <div
-      className="w-screen h-screen bg-cover bg-center items-center justify-center bg-fixed overflow-hidden border-4 "
+      className="w-screen h-screen bg-cover bg-center border-4"
       style={{ backgroundImage: "url('/image/image4.jpeg')" }}
     >
-      <div className="  translate-y-40 h-120 translate-x-90 bg-amber-200 bg-cover rounded-2xl p-7 w-xl justify-center m-3  ">
-        <h1 className="text-2xl text-black text-center font-bold">Login</h1>
-        <form onSubmit={handleSubmit} className="space-y-4 h-60">
-          <img
-            src="/image/8792047.png"
-            alt="Logo"
-            className="w-15 h-15 mx-auto mb-4"
-          />
-          <div className="">
-            <label className="mb-1 block">Name</label>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              className="bg-white w-md rounded-2xl h-12 flex "
-              value={formData.name}
-              onChange={(e) => setFormData({ ...formData, name:e.target.value })}
+      <div className="flex justify-center">
+        <div className="bg-amber-200 rounded-3xl w-xl h-auto mt-40">
+          <h1 className="text-2xl text-black text-center font-bold">Login</h1>
+          <form onSubmit={handleSubmit}>
+            <img
+              src="/image/8792047.png"
+              alt="Logo"
+              className="w-15 h-15 mx-auto"
             />
-          </div>
 
-          <div>
-            <label className="mb-1 block">Password</label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="bg-white w-md rounded-xl h-12"
-              value={formData.password}
-              onChange={(e) => setFormData({ ...formData,password:e.target.value })}
-            />
-          </div>
+            <div className="p-3 ml-3 flex flex-col">
+              <label>Name</label>
+              <input
+                type="text"
+                placeholder="Enter your name"
+                className="bg-white w-md rounded-2xl h-12 pl-4"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+              {nameError && (
+                <p className="text-red-600 text-sm mt-1">{nameError}</p>
+              )}
+            </div>
 
-          {error && <p className="text-red-600 text-center">{error}</p>}
+            <div className="p-3 ml-3 flex flex-col">
+              <label>Password</label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="bg-white w-md rounded-xl h-12 pl-4"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
+              {passwordError && (
+                <p className="text-red-600 text-sm mt-1">{passwordError}</p>
+              )}
+            </div>
 
-          {submitted && (
-            <p className="text-green-700 text-center">
-              submitted successfully...
-            </p>
-          )}
+            {submitted && (
+              <p className="text-green-700 text-center">
+                Submitted successfully...
+              </p>
+            )}
 
-          <br />
-
-          <div className="flex justify-center">
-            <button
-              
-              className=" pr-5 my-6 justify-center bg-amber-700 p-2  rounded-xl hover:bg-blue-400 text-cyan-900 al"
-              type="submit"
-            >
-              {Name}
-           
-            </button>
-          </div>
-        </form>
+            <div className="flex justify-center">
+              <button
+                className="p-3 m-4 bg-amber-600  rounded-xl hover:bg-amber-800 text-cyan-900"
+                type="submit"
+              >
+                {Name}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
